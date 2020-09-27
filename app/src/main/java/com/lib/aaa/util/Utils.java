c5acf22f96ad3adb67f543d25526654a34f261a8
jiangtao <jiangtao@cloudwalk.cn>
2020-09-27 13:41:10 +0800

    更新内容:    1.这是一个测试项目.

---
+++ app/src/main/java/com/lib/aaa/util/Utils.java
@@ -0,0 +1,13 @@
+package com.lib.aaa.util;
+
+/**
+ * Create by jtxuanshui
+ * on 2020/9/27
+ */
+public class Utils {
+    public static final String TAG = Utils.class.getSimpleName();
+
+    public static String getTestInfo(){
+        return "This is a test info.";
+    }
+}
